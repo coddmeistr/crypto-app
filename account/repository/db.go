0bbe25f7dd510332ffa8738a71fc00ce3dedc3e6
maxik12233 <euseew.maxim2015@yandex.ru>
2023-11-19 19:33:38 +0300

    Database connection in docker fixed. Now docker uses not latest golang image. Added hardcoded delay before connectin to a database in account service. Added empty init.sql file just in case

--- account/repository/db.go
+++ account/repository/db.go
@@ -1,18 +1,27 @@
 package repository
 
 import (
+	"time"
+
+	"github.com/maxim12233/crypto-app-server/account/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+// Hard coded gouruting block to wait until postgres container starts
+// Oddly depends_on docker-compose param doesn't work
+// In all test cases 5 seconds were enough for postgres to run
+const HARD_WAIT_FOR_POSTGRES_CONTAINER_START = 5 // in seconds
+
 func InitDB(dbUrl string) (*gorm.DB, error) {
+	time.Sleep(time.Second * HARD_WAIT_FOR_POSTGRES_CONTAINER_START)
 
 	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	//err = db.AutoMigrate(&models.Account{}, &models.Balance{}, &models.Activity{})
+	err = db.AutoMigrate(&models.Account{}, &models.Balance{}, &models.Activity{})
 	if err != nil {
 		return nil, err
 	}
